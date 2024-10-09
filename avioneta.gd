e21936ce8d8e3266076568d036568abd9c470c9c
Matata <matjanikow@gmail.com>
2024-10-09 15:43:38 -0300

    Clase 3

--- avioneta.gd
+++ avioneta.gd
@@ -1,4 +1,17 @@
 extends Node2D
+var speed = 500
 
-func _process(delta): 
-	position.x = position.x - 200 * delta 
+func _process(delta):
+	moverse(delta)
+	#if position.x < 0:
+		#position.x = get_viewport_rect().size.x
+	#if position.x > get_viewport_rect().size.x:
+		#position.x = 0
+	position.x = clamp(position.x, 0, get_viewport_rect().size.x)
+	
+	
+func moverse(delta):
+	if Input.is_action_pressed("Izq"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("Der"):
+		position.x += speed * delta
